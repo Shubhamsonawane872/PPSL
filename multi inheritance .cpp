6c5db9ba6306a204a50e8612a5b7301c7b0a404b
Shubhamsonawane872 <sonawaneshubham5050@gmail.com>
2024-10-08 16:52:08 +0530

    Add files via upload

---
+++ multi inheritance .cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Vehicle{ 
+    public:
+     void vehicle () {
+         cout <<"I am a Vehicle." << endl;
+         
+     }
+}; 
+   class Fourwheeler: public Vehicle{ 
+    public:
+     void fourwheeler(){
+         cout <<"I am a fourwheeler." << endl;
+         
+     }
+};
+  
+class Car:public Fourwheeler{ 
+    public:
+     void car(){
+         cout <<"I am a Car." << endl;
+         
+     }
+};
+
+int main(){
+   Car mycar;
+   
+   mycar.car();
+   mycar.fourwheeler();
+   mycar.vehicle();
+   
+    return 0;
+}
